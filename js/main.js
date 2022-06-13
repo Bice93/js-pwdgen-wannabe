e8cab23cf49a278d558a2ac72ae7bd5e8db89770
Bice93 <bianca.cerulo@hotmail.it>
2022-06-13 17:40:35 +0200

    edit variables

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
-
 // chiediamo all'utente il suo nome
 const userName = prompt ("inserisci il tuo nome");
-// console.log(userName);
+console.log(userName);
 
 // chiediamo all'utente il suo cognome
 const userSurname = prompt ("inserisci il tuo cognome");
-// console.log(userSurname);
+console.log(userSurname);
 
 // chiediamo all'utente il suo colore preferito
 const favoriteColor = prompt ("inserisci il tuo colore preferito");
+console.log(favoriteColor);
+
+
+let passwordUser = userName + userSurname + favoriteColor + "22";
+console.log(passwordUser);
+document.getElementById("password-user").innerHTML = passwordUser;
+
+
+// // chiediamo all'utente il suo nome
+// const userName = prompt ("inserisci il tuo nome");
+// console.log(userName);
+
+// // chiediamo all'utente il suo cognome
+// const userSurname = prompt ("inserisci il tuo cognome");
+// console.log(userSurname);
+
+// // chiediamo all'utente il suo colore preferito
+// const favoriteColor = prompt ("inserisci il tuo colore preferito");
 // console.log(favoriteColor);
 
-document.getElementById("user-name").innerHTML = userName;
-document.getElementById("user-surname").innerHTML = userSurname;
-document.getElementById("favorite-color").innerHTML = favoriteColor;
+
+
+// document.getElementById("user-name").innerHTML = userName;
+// document.getElementById("user-surname").innerHTML = userSurname;
+// document.getElementById("favorite-color").innerHTML = favoriteColor;
+
